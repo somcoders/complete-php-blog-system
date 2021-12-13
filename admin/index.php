41d8ab69707d2307c47f43d3c5a8b4b66cdc54b0
Abdifatahz <faahid44@gmail.com>
2021-12-13 13:23:53 +0300

    Fix bug edit|delete broken links.

--- admin/index.php
+++ admin/index.php
@@ -105,8 +105,8 @@
                                 <td><?=  capitalize(($category->name)); ?></td>
                                 <td><?=  clean_date($post->created_at); ?></td>
                                 <td><?=  get_status($post->status); ?></td>
-                                <td><a href="post.php?post_id=<?= escape($post->id); ?>">Update</a></td>
-                                <td><a href="post.php?post_id=<?= escape($post->id); ?>&action=del">Delete</a></td>
+                                <td><a href="edit-post.php?post_id=<?= escape($post->id); ?>">Edit</a></td>
+                                <td><a href="edit-post.php?post_id=<?= escape($post->id); ?>&action=del">Delete</a></td>
                             </tr>
                             <?php } ?>
                  
